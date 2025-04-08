fa84551a46f13393f6e6e461f9e913d5c76745d6
Theo Snyman <itstheosnyman@gmail.com>
2025-04-08 20:26:00 +0200

    Event emission

--- src/components/MovieEntry.vue
+++ src/components/MovieEntry.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-  // props allow us to define the properties that has to be passed down from a parent component (MoviesList in this case)
+  // props allow us to define what the component is expecting to be passed down from the parent (MoviesList in this case)
   props: {
     // vue will highlight if we pass something down that doesn't match the "prototypes" below. default values can also be set to properties for the event where no props were passed down
     movies: {
@@ -8,13 +8,12 @@ export default {
       required: true,
       // default: []
     },
-    favoriteMovie: {
-      type: Function,
-      required: true,
-      // default: []
-    },
   },
+  // new to vue 3 is the ability to document a childs emits in the script. emits define what a child will be sending up, basically a signal conveying that the child wants something to be changed, once changed the data is sent back down
+  emits: ['favoriteMovie'],
 }
+
+// both props and emits require components to be relatively closely related in order to avoid having to pass data or signals thorugh multiple levels of components
 </script>
 
 <template>
@@ -26,7 +25,8 @@ export default {
       <ul v-else>
         <li v-for="character in movie.characters">{{ character }}</li>
       </ul>
-      <button @click="favoriteMovie(movie.name)">Favorite Movie</button>
+      <!-- this button will now when clicked send a signal up to the parent that something needs to change, the parent will capture this signal, do something, and then send the result back down to the child -->
+      <button @click="$emit('favoriteMovie', movie.name)">Favorite Movie</button>
     </div>
   </div>
 </template>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -33,7 +33,7 @@ export default {
     <h2>Movies List</h2>
     <div>
       <!-- bind prop & method in this case to data in our parent component. pay attention to when you are passing down to many props because then it might be better to "destructure" the props and only pass down individual properties of a larger object for instance, that larger object can also be refined and massaged with a computed property -->
-      <MovieEntry :movies="movies" :favoriteMovie="favoriteMovie" />
+      <MovieEntry :movies="movies" @favoriteMovie="favoriteMovie" />
     </div>
     <h3>Favorited Movies</h3>
     <p v-if="favoriteMovies.length === 0">No favorited movies in the system 😓</p>
