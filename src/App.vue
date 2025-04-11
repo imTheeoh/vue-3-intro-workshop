c1e416e38f4a499a949a9ac26a03da5b3b60f3a5
Theo Snyman <itstheosnyman@gmail.com>
2025-04-11 17:17:00 +0200

    Lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,25 @@
-<!-- <script> manages js -->
 <script>
-import RandomCounter from './components/RandomCounter.vue'
-import LoopingUsers from './components/LoopingUsers.vue'
-import MoviesList from './components/MoviesList.vue'
-import FormCapture from './components/FormCapture.vue'
-import BaseLayout from './components/BaseLayout.vue'
-
+// api url -> https://pokeapi.co/api/v2/pokemon?limit=151
 export default {
-  // components option allows us to register components that we want to use throughout our app or within other components
-  components: {
-    RandomCounter,
-    LoopingUsers,
-    MoviesList,
-    FormCapture,
-    BaseLayout,
+  data: () => ({
+    pokedex: null,
+  }),
+  methods: {
+    async fetchPokemon() {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      const pokemonData = await response.json()
+      console.log(`${pokemonData.results.length} Pokemon loaded into the Pokédex`)
+
+      this.pokedex = pokemonData.results
+    },
   },
-  // reactive data properties
-  data() {
-    return {}
+  // the created lifecycle hook ensures our api call has already gone and fetched the data before the component gets rendered so the data is immediately visible
+  created() {
+    this.fetchPokemon()
   },
-  // use computed when you want to obtain a value based on other reactive data. results are cached and recalculated only when necessary. computed manages data
-  computed: {},
-  // use watch when you want to watch a specific value and perform side effects. good for reacting to state changes without returning a value. watch manages actions that should happen based on state
-  watch: {},
-  // methods are simply methods that operate on reactive data properties
-  methods: {},
 }
 </script>
 
-<!-- <template> manages html -->
 <template>
-  <BaseLayout>
-    <template v-slot:section-1>
-      <RandomCounter />
-    </template>
-    <template v-slot:section-2>
-      <LoopingUsers />
-    </template>
-    <template v-slot:section-3>
-      <MoviesList />
-    </template>
-    <template v-slot:footer>
-      <FormCapture />
-    </template>
-  </BaseLayout>
+  <pre>{{ pokedex }}</pre>
 </template>
-
-<!-- <style> manages css -->
-<style></style>
---
+++ src/OldApp.vue
@@ -0,0 +1,50 @@
+<!-- <script> manages js -->
+<script>
+import RandomCounter from './components/RandomCounter.vue'
+import LoopingUsers from './components/LoopingUsers.vue'
+import MoviesList from './components/MoviesList.vue'
+import FormCapture from './components/FormCapture.vue'
+import BaseLayout from './components/BaseLayout.vue'
+
+export default {
+  // components option allows us to register components that we want to use throughout our app or within other components
+  components: {
+    RandomCounter,
+    LoopingUsers,
+    MoviesList,
+    FormCapture,
+    BaseLayout,
+  },
+  // reactive data properties
+  data() {
+    return {}
+  },
+  // use computed when you want to obtain a value based on other reactive data. results are cached and recalculated only when necessary. computed manages data
+  computed: {},
+  // use watch when you want to watch a specific value and perform side effects. good for reacting to state changes without returning a value. watch manages actions that should happen based on state
+  watch: {},
+  // methods are simply methods that operate on reactive data properties
+  methods: {},
+}
+</script>
+
+<!-- <template> manages html -->
+<template>
+  <BaseLayout>
+    <template v-slot:section-1>
+      <RandomCounter />
+    </template>
+    <template v-slot:section-2>
+      <LoopingUsers />
+    </template>
+    <template v-slot:section-3>
+      <MoviesList />
+    </template>
+    <template v-slot:footer>
+      <FormCapture />
+    </template>
+  </BaseLayout>
+</template>
+
+<!-- <style> manages css -->
+<style></style>
