7f183ce61235de4b4918396fcf0c404319d63419
Theo Snyman <itstheosnyman@gmail.com>
2025-04-19 21:17:56 +0200

    Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,27 @@
-<!-- <script> manages js -->
-<script>
-import RandomCounter from './components/RandomCounter.vue'
-import LoopingUsers from './components/LoopingUsers.vue'
-import MoviesList from './components/MoviesList.vue'
-import FormCapture from './components/FormCapture.vue'
-import BaseLayout from './components/BaseLayout.vue'
+<template>
+  <h1>{{ helloWorld }}</h1>
+  <p>My name is {{ myName }}.</p>
+</template>
 
+<script>
 export default {
-  // components option allows us to register components that we want to use throughout our app or within other components
-  components: {
-    RandomCounter,
-    LoopingUsers,
-    MoviesList,
-    FormCapture,
-    BaseLayout,
-  },
-  // reactive data properties
-  data() {
+  setup() {
+    // The setup lifecycle is the earliest point in the lifecycle of a component, ideal for asynchronous operations where data has to be fetched etc...
+
+    // The composition api is basically vanilla javascript because composition api gives us freedom and flexibility. Options api provides more consistency but lack freedom and flexibility
+
+    const helloWorld = 'Hello World!'
+    const myName = 'Theo'
+
     return {
-      currentPage: null,
+      // Everything we want to expose has to be returned manually in composition api
+      helloWorld,
+      myName,
     }
-  },
-  // use computed when you want to obtain a value based on other reactive data. results are cached and recalculated only when necessary. computed manages data
-  computed: {
-    renderPage() {
-      return this.currentPage
-    },
-  },
-  // use watch when you want to watch a specific value and perform side effects. good for reacting to state changes without returning a value. watch manages actions that should happen based on state
-  watch: {},
-  // methods are simply methods that operate on reactive data properties
-  methods: {
-    setPageRandomCounter() {
-      this.currentPage = 'RandomCounter'
-    },
-    setPageLoopingUsers() {
-      this.currentPage = 'LoopingUsers'
-    },
-    setPageMoviesList() {
-      this.currentPage = 'MoviesList'
-    },
-    setPageFormCapture() {
-      this.currentPage = 'FormCapture'
-    },
+
+    // ASYNC OPERATIONS SHOULD NOT BE RUN IN SETUP INSIDE THE APP.VUE SINCE THE ROOT COMPONENT NEEDS TO BE IN A CONSTANTLY STABLE STATE. COMPONENTS CAN BE HELD ONTO A BIT AND GIVEN FALLBACKS IF THEIR ASYNC OPERATIONS FAIL BUT THE ROOT COMPONENT DOES NOT HAVE THE SAME LUXURIES
   },
 }
 </script>
 
-<!-- <template> manages html -->
-<template>
-  <nav>
-    <a href="#" @click.prevent="setPageRandomCounter">Random Counter</a>
-    <a href="#" @click.prevent="setPageLoopingUsers">Looping Users</a>
-    <a href="#" @click.prevent="setPageMoviesList">Movies List</a>
-    <a href="#" @click.prevent="setPageFormCapture">Form Capture</a>
-  </nav>
-  <component :is="renderPage" />
-</template>
-
-<!-- <style> manages css -->
 <style></style>
---
+++ src/NewestOldApp.vue
@@ -0,0 +1,62 @@
+<!-- <script> manages js -->
+<script>
+import RandomCounter from './components/RandomCounter.vue'
+import LoopingUsers from './components/LoopingUsers.vue'
+import MoviesList from './components/MoviesList.vue'
+import FormCapture from './components/FormCapture.vue'
+import BaseLayout from './components/BaseLayout.vue'
+
+export default {
+  // components option allows us to register components that we want to use throughout our app or within other components
+  components: {
+    RandomCounter,
+    LoopingUsers,
+    MoviesList,
+    FormCapture,
+    BaseLayout,
+  },
+  // reactive data properties
+  data() {
+    return {
+      currentPage: null,
+    }
+  },
+  // use computed when you want to obtain a value based on other reactive data. results are cached and recalculated only when necessary. computed manages data
+  computed: {
+    renderPage() {
+      return this.currentPage
+    },
+  },
+  // use watch when you want to watch a specific value and perform side effects. good for reacting to state changes without returning a value. watch manages actions that should happen based on state
+  watch: {},
+  // methods are simply methods that operate on reactive data properties
+  methods: {
+    setPageRandomCounter() {
+      this.currentPage = 'RandomCounter'
+    },
+    setPageLoopingUsers() {
+      this.currentPage = 'LoopingUsers'
+    },
+    setPageMoviesList() {
+      this.currentPage = 'MoviesList'
+    },
+    setPageFormCapture() {
+      this.currentPage = 'FormCapture'
+    },
+  },
+}
+</script>
+
+<!-- <template> manages html -->
+<template>
+  <nav>
+    <a href="#" @click.prevent="setPageRandomCounter">Random Counter</a>
+    <a href="#" @click.prevent="setPageLoopingUsers">Looping Users</a>
+    <a href="#" @click.prevent="setPageMoviesList">Movies List</a>
+    <a href="#" @click.prevent="setPageFormCapture">Form Capture</a>
+  </nav>
+  <component :is="renderPage" />
+</template>
+
+<!-- <style> manages css -->
+<style></style>
